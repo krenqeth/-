3de25ad3fb58e9e3e231fb979f95e90561bb9bbd
krenqeth <wonderslurp@mail.ru>
2026-03-20 14:36:18 +0500

    Update fraction.cpp

--- DROB1/fraction.cpp
+++ DROB1/fraction.cpp
@@ -1,60 +1,83 @@
-#include "Fraction.hpp"
+##include "fraction.hpp"
 
-Fraction::Fraction(int num, int den) : numerator(num), denominator(den)
+// ===== КОНСТРУКТОР =====
+Fraction::Fraction(int num, int den)
 {
+    if (den == 0)
+    {
+        std::cout << "Error: denominator = 0! Set to 1\n";
+        den = 1;
+    }
 
-
+    numerator = num;
+    denominator = den;
 }
 
+// ===== SET =====
+void Fraction::Setnumerator(int num)
+{
+    numerator = num;
+}
+
+void Fraction::Setdenominator(int den)
+{
+    if (den == 0)
+    {
+        std::cout << "Error: denominator = 0! Set to 1\n";
+        denominator = 1;
+    }
+    else
+    {
+        denominator = den;
+    }
+}
+
+// ===== GET =====
+int Fraction::Getnumerator() const
+{
+    return numerator;
+}
+
+int Fraction::Getdenominator() const
+{
+    return denominator;
+}
+
+// ===== ОПЕРАЦИИ =====
 Fraction Fraction::Sum(const Fraction& other)
 {
-	int newNumerator = numerator * other.denominator + other.numerator * denominator;
-	int newDenominator = denominator * other.denominator;
-
-	return Fraction(newNumerator, newDenominator);
+    return Fraction(
+        numerator * other.denominator + other.numerator * denominator,
+        denominator * other.denominator
+    );
 }
 
 Fraction Fraction::Subtract(const Fraction& other)
 {
-	int newNumerator = numerator * other.denominator - other.numerator * denominator;
-	int newDenominator = denominator * other.denominator;
-
-	return Fraction(newNumerator, newDenominator);
+    return Fraction(
+        numerator * other.denominator - other.numerator * denominator,
+        denominator * other.denominator
+    );
 }
 
 Fraction Fraction::Multiply(const Fraction& other)
 {
-	int newNumerator = numerator * other.numerator;
-	int newDenominator = denominator * other.denominator;
-
-	return Fraction(newNumerator, newDenominator);
+    return Fraction(
+        numerator * other.numerator,
+        denominator * other.denominator
+    );
 }
 
 Fraction Fraction::Divide(const Fraction& other)
 {
-	int newNumerator = numerator / other.numerator;
-	int newDenominator = denominator / other.denominator;
-
-
-	return Fraction(newNumerator, newDenominator);
+    return Fraction(
+        numerator * other.denominator,
+        denominator * other.numerator
+    );
 }
 
-void Fraction::Setnumerator(int one)
+// ===== PRINT =====
+void Fraction::Print() const
 {
-	if (numerator < 0)
-	{
-		std::cout << "Number can't be zero";
-		return;
-	}
-}
-
-void Fraction::Setdenominator(int two)
-{
-	if (numerator < 0)
-	{
-		std::cout << "Number can't be zero";
-		return;
-	}
-
-
+    std::cout << numerator << "/" << denominator;
 }
